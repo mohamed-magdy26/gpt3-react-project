880c2f814d241d7b54d0121b565fb13848dd0796
mohamed magdy <mohamed.magdy0598@gmail.com>
2023-05-15 23:17:35 +0300

    compressing images and add lazy loading

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './possibility.css';
-import possibilityImage from '../../assets/possibility.png';
+import possibilityImage from '../../assets/possibility.avif';
 
 const Possibility = () => {
   return (
-    <div className="gpt3__possibility section__padding" id="possibility">
+    <div
+      className="gpt3__possibility section__padding"
+      id="possibility"
+    >
       <div className="container">
         <div className="gpt3__possibility-image">
-          <img src={possibilityImage} alt="possibility" />
+          <img
+            src={possibilityImage}
+            alt="possibility"
+            loading="lazy"
+          />
         </div>
         <div className=" gpt3__possibility-content">
           <p>Request Early Access to Get Started</p>
@@ -15,10 +22,10 @@ const Possibility = () => {
             The possibilities are beyond your imagination
           </h3>
           <p>
-            Yet bed any for travelling assistance indulgence unpleasing. Not
-            thoughts all exercise blessing. Indulgence way everything joy
-            alteration boisterous the attachment. Party we years to order allow
-            asked of.
+            Yet bed any for travelling assistance indulgence
+            unpleasing. Not thoughts all exercise blessing. Indulgence
+            way everything joy alteration boisterous the attachment.
+            Party we years to order allow asked of.
           </p>
           <p>Request Early Access to Get Started</p>
         </div>
